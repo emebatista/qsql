c66e968c3b2c78d3ced146517e347bcd438d0b6f
Emerson D Batista <emebatista@hotmail.com>
2022-08-29 13:45:56 -0300

    Removido o arquivo.

--- src/gera_app.sh
+++
@@ -1,5 +0,0 @@
-ng build
-cd dist
-rm "/Users/emebatista/OneDrive/TOTVS/_Polimaquinas_Local/polimaquinas/Polimaquinas/customizacoes/especificos - app/qsqlweb.app" 
-zip -r "/Users/emebatista/OneDrive/TOTVS/_Polimaquinas_Local/polimaquinas/Polimaquinas/customizacoes/especificos - app/qsqlweb.app" qsqlweb
-cd ..
